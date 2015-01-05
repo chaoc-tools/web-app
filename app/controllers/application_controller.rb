04c14c40131bc53fe327010509a5e5d9a58f3256
Anders Ramsay <andersr@gmail.com>
2015-01-05 17:33:01 -0500

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authenticate
+
+  protected
+
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["HT_USR"] && password == ENV["HT_PWD"]
+      end
+    end
+
 end
