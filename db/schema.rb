8cf4ffd7d3be839db63a3845bab1b768a729144c
Anders Ramsay <andersr@gmail.com>
2015-01-07 14:52:25 -0500

    google omniauth in progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107014258) do
+ActiveRecord::Schema.define(version: 20150107143956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 20150107014258) do
     t.integer  "building_id"
     t.integer  "user_id"
     t.integer  "votes_collected"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
